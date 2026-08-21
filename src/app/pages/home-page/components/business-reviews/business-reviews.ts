a78f8721e64e1b77489ae952318d55fffa24663d
Leslie Cunningham <LeslieMCunningham@outlook.com>
2026-08-21 17:32:56 -0400

    Homepage Edits

--- src/app/pages/home-page/components/business-reviews/business-reviews.ts
+++ src/app/pages/home-page/components/business-reviews/business-reviews.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
 export class BusinessReviews {
   reviews = [
     { projectType:"Demolition", name: "Tanna Combs Crouch", stars: 5, reviewDesc: "We love everything you all have done and highly recommend this company." }, 
-    { projectType:"Demolition", name: "Tanna Combs Crouch", stars: 5, reviewDesc: "We love everything you all have done and highly recommend this company." } ,
+    { projectType:"Repair & Restoration", name: "All Spark Electric", stars: 5, reviewDesc: "Rigid Contracting, we appreciate your assistance on this project, which involved multiple repairs to restore its original appearance after the removal of the old service. We look forward to collaborating with you again in the future..." } ,
     { projectType:"Demolition", name: "Tanna Combs Crouch", stars: 5, reviewDesc: "We love everything you all have done and highly recommend this company." } 
   ]
 }
