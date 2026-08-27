c0a4fb65d812df457ef6ab69f358a0a4cfe42daa
Leslie Cunningham <LeslieMCunningham@outlook.com>
2026-08-27 17:24:32 -0400

    Slight Prettify!

--- src/app/pages/contact-us/contactus-card/contactus-card.ts
+++ src/app/pages/contact-us/contactus-card/contactus-card.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './contactus-card.html',
   styleUrl: './contactus-card.css',
 })
-export class ContactusCard {}
+export class ContactusCard {
+  owners = [
+    { name: "Justice Reed", number:"(859) 595-8172", email: "justice.reed@rigidcontractingky.com" },
+    { name: "Austin Richards", number:"(606) 484-0606", email: "austin.richards@rigidcontractingky.com" }
+  ]
+}
